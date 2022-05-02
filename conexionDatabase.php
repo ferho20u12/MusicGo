0c8e039180709abb89c42599b3a794a8552433f4
fernando <ferho20u12@gmail.com>
2022-05-01 20:23:11 -0500

    1-5-22

--- conexionDatabase.php
+++ conexionDatabase.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "ec2-52-5-110-35.compute-1.amazonaws.com";
-    $dbname = "d6l5dns95ogceh";
-    $user = "uszpshrizldhxe";
-    $pass = "a9153984d55479c6f963f843397c13228666d9208c720f6c887e9bd60eaa521c";
+    $host = "localhost";
+    $dbname = "BaseDeDatosProyecto";
+    $user = "postgres";
+    $pass = "123";
 
     $conexion= pg_connect("host='$host' dbname='$dbname' user='$user' password='$pass'");
 ?>
